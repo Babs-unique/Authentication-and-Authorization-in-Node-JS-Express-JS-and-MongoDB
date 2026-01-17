5d3b37027abaa14a08d107a13e019179bdbc22c0
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-01-17 19:43:56 +0100

    Added paystack Initialization

---
+++ src/payments/paystack.js
@@ -0,0 +1,69 @@
+const axios = require('axios');
+const User = require('../models/auth.models');
+
+
+const paystackInitiatePayment = async (req, res) => {
+    const { amount, redirectUrl, accountNumber, currency } = req.body;
+    const { userId } = req.user;
+
+    if(!userId){
+        return res.status(400).json({message : "User not found"});
+    }
+    if(!amount || !redirectUrl || !currency || !accountNumber){
+        return res.status(400).json({message : "All fields are required"});
+    }
+    try {
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(400).json({message : "User does not exist"});
+        }
+        const txRef = `PSK-${Date.now()} - ${userId}`;
+        const moneyInKobo = Math.round(amount * 100);
+
+        const payload = {
+            email: user.email,
+            amount: amountInKobo,
+            reference: reference,
+            callback_url: callbackUrl || process.env.PAYSTACK_CALLBACK_URL,
+            metadata: {
+                userId: userId,
+                custom_fields: [
+                {
+                    display_name: "Customer Name",
+                    variable_name: "customer_name",
+                    value: user.name,
+                },
+                ],
+    },
+        }
+        const response = await axios.post(
+            "https://api.paystack.co/transaction/initialize",
+            payload,
+        {
+            headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+            "Content-Type": "application/json",
+            },
+        }
+        )
+        if(response.data.status){
+            return res.status(200).json({
+                message: "Payment initialized successfully",
+                authorizationUrl: response.data.data.authorization_url,
+                accessCode: response.data.data.access_code,
+                reference: reference,
+            })}else{
+                return res.status(400).json({
+                    message: "Payment initialization failed",
+                    error: response.data.message
+                });
+            }
+    } catch (error) {
+        console.error("Error in paystackInitiatePayment:", error);
+        return res.status(500).json({message : "Internal Server Error"});
+    }
+}
+
+
+
+module.exports = { paystackInitiatePayment };
--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const myAuth = require('../config/auth');
 const { createWallet, getAllWallets, transferFunds } = require('../controller/wallet.controller');
 const { redirectUrl } = require('../payments/flutterwave');
+const { paystackInitiatePayment } = require('../payments/paystack');
 
 
 const walletRouter = express.Router();
@@ -11,6 +12,6 @@ walletRouter.post('/create-wallet' ,myAuth , createWallet);
 walletRouter.get('/all-wallets' , myAuth , getAllWallets);
 walletRouter.post('/transfer-funds' , myAuth , transferFunds);
 walletRouter.post('/flutterwave-payment' , myAuth , redirectUrl);
-
+walletRouter.get('/paystack-payment' , myAuth, paystackInitiatePayment );
 
 module.exports = walletRouter;
